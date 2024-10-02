6144be3fec2e2c53cc3923b699e05c7133b8e06c
Kurvel <143402591+Kurvel@users.noreply.github.com>
2024-10-02 15:28:08 +0200

    first commit

---
+++ src/main/java/com/sensor_backend/sensor_backend/SensorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sensor_backend.sensor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorBackendApplication.class, args);
+	}
+
+}
